ec5d15c6d2aa5abd0d7656e161c9e563b02990f9
USGinger <sales@usginger.com>
2026-06-20 13:33:21 -0400

    Refactor service worker for caching and activation

--- sw.js
+++ sw.js
@@ -2,19 +2,36 @@ const CACHE_NAME = 'stasis-reader-v1';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/daily-log.html'
+  '/daily-log.html',
+  '/pwa-manifest.json'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then((cache) => cache.addAll(urlsToCache))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then((response) => {
+        return response || fetch(event.request);
+      })
   );
 });
